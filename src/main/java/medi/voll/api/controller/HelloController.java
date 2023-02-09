25914b7e510bdf7889040c98f65eea79f2691381
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-02-09 01:43:31 -0300

    Hellow World com spring

--- src/main/java/medi/voll/api/controller/HelloController.java
+++ src/main/java/medi/voll/api/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String olaMundo(){
-        return "Hello World, Spriiiing!";
+        return "Hello World, Spriiiiing!";
     }
 }
